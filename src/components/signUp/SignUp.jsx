b1fc49c3f2254bb959cd4bdf71cd198012a3d8ee
Gaurav Kumar <gaurav.kumar4@incedoinc.com>
2024-02-28 16:14:09 +0530

    added SSO

--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./SignUp.css";
 import image from "../../assets/patternImg.f93bd17c.svg";
 import { Link } from "react-router-dom";
- 
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 export default function SignUp() {
   const [currValue, setCurrValue] = useState({
@@ -28,26 +28,24 @@ export default function SignUp() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const passw=  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    const enteredPass=currValue.password
-    
-    if(!passw.test(enteredPass))
-    {
-        setIsFormDataValid(false)
-        return
+    const passw = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    const enteredPass = currValue.password;
+
+    if (!passw.test(enteredPass)) {
+      setIsFormDataValid(false);
+      return;
     }
-    setIsFormDataValid(true)
+    setIsFormDataValid(true);
     console.log(currValue);
     setCurrValue({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
-    state: "",
-    city: "",
-    address: "",
-    })
-    
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      state: "",
+      city: "",
+      address: "",
+    });
   };
 
   return (
@@ -87,7 +85,6 @@ export default function SignUp() {
               </div>
             </div>
             <div className="input email ">
-              
               <input
                 type="email"
                 id="email"
@@ -109,7 +106,12 @@ export default function SignUp() {
                 required
                 minLength={8}
               />
-              {!isFormDataValid && <p className="error-class">Password must consist a lowercase,uppercase, special character and a digit.</p>}
+              {!isFormDataValid && (
+                <p className="error-class">
+                  Password must consist a lowercase,uppercase, special character
+                  and a digit.
+                </p>
+              )}
             </div>
             <div className="city-container input">
               <div>
@@ -145,15 +147,36 @@ export default function SignUp() {
                 cols="30"
                 rows="5"
                 placeholder="Address"
-              >HomeIcon</textarea>
+              >
+                HomeIcon
+              </textarea>
             </div>
             <div className="button-container">
               <button className="submit">Register</button>
             </div>
           </form>
         </div>
+        <div className="under-line">
+          <p>Or</p>
+          <GoogleOAuthProvider  clientId="1006719320012-bpprguisgq6oq9o716i7lrd14s1ol0dr.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed")
+            }}
+          />
+          
+        </GoogleOAuthProvider>
+        </div>
+
         <p>
-          Already have account? <Link className="link-text" to="/login"> Click Here</Link>
+          Already have account?{" "}
+          <Link className="link-text" to="/login">
+            {" "}
+            Click Here
+          </Link>
         </p>
       </div>
     </div>
