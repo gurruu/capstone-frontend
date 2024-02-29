75531fba2198617b0265987f9593b0fbab882b41
Priya Pal <priya.pal1@incedoinc.com>
2024-02-29 22:30:35 +0530

    navbar button added

--- src/components/homePage/navBar.jsx
+++ src/components/homePage/navBar.jsx
@@ -15,6 +15,32 @@ const Navbar = () => {
           DashBoard
         </Link>
       </div>
+      
+      <div className="navbar-left">
+      <button as="button" class="tab__bJxhD" tabindex="0" cur-idx="0" is-selected=" "> 
+        <div>Investment Feed</div>
+        
+        
+        </button>
+        <button as="button" class="tab__bJxhD" tabindex="0" cur-idx="0" is-selected=" "> 
+        <div>Calculator</div>
+        
+        
+        </button>
+        <button as="button" class="tab__bJxhD" tabindex="0" cur-idx="0" is-selected=" "> 
+        <div>FAQs</div>
+        
+        {/* <div class="tab__bJxhD" is-selected=" " style="visibility: hidden; height: 0px; padding: 0px;">
+          Investment Feed</div> */}
+        </button>
+        
+        
+      </div>
+        
+  
+      
+      
+      
       <div className="navbar-end">
         <div className="navbar-item">
           <Link to="/signin" className="button is-primary">
