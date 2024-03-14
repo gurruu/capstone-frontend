9a1b7457ef2bc65ad6eb20e67f7a9ceb6896a30d
Ashish Kumar <ashish.kumar4@incedoinc.com>
2024-03-14 15:45:52 +0530

    advisor dashboard

---
+++ src/components/AdvisorInterface/Requests/AdRequestPage.jsx
@@ -0,0 +1,28 @@
+import './adRequestPage.css';
+import React ,{useState} from 'react'
+import AdSideBar from '../AdSideBar'
+import AdHeader from '../AdHeader'
+import AdRequestTable from './AdRequestTable'
+
+function AdRequestPage() {
+    const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+    const OpenSidebar = () => {
+        setOpenSidebarToggle(!openSidebarToggle);
+      };
+  
+  return (
+    
+    <div className='grid-container '>
+    <AdHeader OpenSidebar={OpenSidebar} />
+    <AdSideBar openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}/>
+        <div className=''>
+        <AdRequestTable/>
+        </div>
+        
+    </div>
+    
+  );
+}
+
+export default AdRequestPage;
